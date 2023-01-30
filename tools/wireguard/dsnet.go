efa5717a657eb7fcef38f8a259210213bb0c0c2c
Antonio Cheong <acheong@student.dalat.org>
2023-01-30 15:06:28 +0800

    Mistaken re-init

--- tools/wireguard/dsnet.go
+++ tools/wireguard/dsnet.go
@@ -1,27 +1,9 @@
 package wireguard
 
 import (
-	"strings"
-	"time"
-
 	_ "github.com/RunawayVPN/dsnet"
-	"github.com/spf13/viper"
 )
 
 func init() {
-	// Environment variable handling.
-	viper.AutomaticEnv()
-	viper.SetEnvPrefix("DSNET")
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	viper.SetDefault("config_file", "/etc/dsnetconfig.json")
-	viper.SetDefault("fallback_wg_bing", "wireguard-go")
-	viper.SetDefault("listen_port", 51820)
-	viper.SetDefault("interface_name", "dsnet")
-
-	// if last handshake (different from keepalive, see https://www.wireguard.com/protocol/)
-	viper.SetDefault("peer_timeout", 3*time.Minute)
-
-	// when is a peer considered gone forever? (could remove)
-	// viper.SetDefault("peer_expiry", 28*time.Hour*24)
 }
