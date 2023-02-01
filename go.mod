a531eac8cd711e14bc3e362c1c0e9d408be978d9
Antonio Cheong <acheong@student.dalat.org>
2023-02-01 22:08:40 +0800

    Fetch network interface

--- go.mod
+++ go.mod
@@ -10,6 +10,37 @@ require (
 
 require (
 	github.com/acheong08/crystals-go v0.0.0-20221113173009-20f37d186766 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/josharian/native v1.0.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mdlayher/genetlink v1.2.0 // indirect
+	github.com/mdlayher/netlink v1.6.0 // indirect
+	github.com/mdlayher/socket v0.2.3 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.13.0 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
+	github.com/vishvananda/netlink v1.1.0 // indirect
+	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
 	golang.org/x/crypto v0.2.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29 // indirect
 	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.zx2c4.com/wireguard v0.0.0-20220407013110-ef5c587f782d // indirect
+	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20220504211119-3d4a969bb56b // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- tools/wireguard/dsnet.go
+++ tools/wireguard/dsnet.go
@@ -1,5 +1,92 @@
 package wireguard
 
 import (
-	_ "github.com/RunawayVPN/dsnet"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+
+	dsnet "github.com/RunawayVPN/dsnet/cmd/cli"
 )
+
+func init() {
+	// Check if dsnet configuration exists
+	_, err := dsnet.MustLoadConfigFile()
+	if err != nil {
+		dsnet.Init()
+		// Necessary edits:
+		// "PostUp": "iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o <main network interface> -j MASQUERADE",
+		// "PostDown": "iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o <main network interface> -j MASQUERADE",
+		// 	"Networks": [
+		//     "0.0.0.0/0",
+		//     "::/0"
+		// ],
+
+		// Read /etc/dsnetconfig.json
+		config, err := os.ReadFile("/etc/dsnetconfig.json")
+		if err != nil {
+			panic(err)
+		}
+		// Parse JSON
+		var data map[string]interface{}
+		err = json.Unmarshal(config, &data)
+		if err != nil {
+			panic(err)
+		}
+		// Get main network interface
+		main_interface, err := get_main_network_interface()
+		if err != nil {
+			panic(err)
+		}
+		// Edit PostUp and PostDown
+		data["PostUp"] = fmt.Sprintf("iptables -A FORWARD -i %%i -j ACCEPT; iptables -A FORWARD -o %%i -j ACCEPT; iptables -t nat -A POSTROUTING -o %s -j MASQUERADE", main_interface)
+		data["PostDown"] = fmt.Sprintf("iptables -D FORWARD -i %%i -j ACCEPT; iptables -D FORWARD -o %%i -j ACCEPT; iptables -t nat -D POSTROUTING -o %s -j MASQUERADE", main_interface)
+		// Edit Networks
+		data["Networks"] = []string{"0.0.0.0/0", "::/0"}
+		// Write to /etc/dsnetconfig.json
+		config, err = json.MarshalIndent(data, "", "  ")
+		if err != nil {
+			panic(err)
+		}
+		os.WriteFile("/etc/dsnetconfig.json", config, 0644)
+	}
+}
+
+// Get the main network interface
+func get_main_network_interface() (string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	for _, i := range interfaces {
+		if i.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if i.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := i.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return i.Name, nil
+		}
+	}
+	return "", fmt.Errorf("network interface not found")
+}
